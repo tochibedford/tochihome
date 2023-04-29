50faa9c4989189f1b1fbc150e512729ed652b78e
tochibedford <musicofbyte@gmail.com>
2023-04-29 19:39:28 +0100

    feat: switched to framer motion for transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,36 +5,39 @@ import '@styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function App({ Component, pageProps }: AppProps) {
   const location = useRouter()
-  const [currentOpacity, setCurrentOpacity] = useState(1)
-
-  useEffect(() => {
-    setCurrentOpacity(1)
-    setTimeout(() => {
-      setCurrentOpacity(0)
-    }, 1000)
-
-
-  }, [Component])
   return (
     <>
       <div className="noiseFilter"></div>
-      <div style={{ opacity: 1 - currentOpacity }} >
-
+      <AnimatePresence mode='wait' initial={false}>
         <Navbar />
-        <ErrorBoundary fallback={<div>There was an Error Loading this page</div>}>
-          <Component {...pageProps} />
-        </ErrorBoundary>
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.6 } }}
+          exit={{ opacity: 0 }}
+
+        >
+          <ErrorBoundary fallback={<div>There was an Error Loading this page</div>}>
+            <Component {...pageProps} />
+          </ErrorBoundary>
+        </motion.div>
         <Footer />
-      </div>
-      <video src="./vhs.mp4" playsInline muted autoPlay loop className="vhsFilter" />
-      <video src="./vhsOptim.mp4" playsInline muted autoPlay loop className="vhsFilter" />
-      <video src="./vhsOptim2.mp4" playsInline muted autoPlay loop className="vhsFilter reducedVisibility" />
-      {/* <div className={styles.}> */}
-      <video src="./lightLeak.mp4" style={{ opacity: currentOpacity }} playsInline muted autoPlay loop className="vhsFilter transitionLightLeak" />
-      {/* </div> */}
+        <video src="./vhs.mp4" playsInline muted autoPlay loop className="vhsFilter" />
+        <video src="./vhsOptim.mp4" playsInline muted autoPlay loop className="vhsFilter" />
+        <video src="./vhsOptim2.mp4" playsInline muted autoPlay loop className="vhsFilter reducedVisibility" />
+        <motion.div
+          key={location.pathname + "newKey"}
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0, transition: { duration: 0.9 } }}
+          exit={{ opacity: 1 }}
+        >
+          <video src="./lightLeak.mp4" playsInline muted autoPlay loop className="vhsFilter transitionLightLeak" />
+        </motion.div>
+      </AnimatePresence>
     </>
   )
 }
